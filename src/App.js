170cad7a7419951d922cf3b7adf48491bdf00244
Chan Woo SHIM <chanwooshim@Chans-MacBook-Air.local>
2024-08-18 15:22:25 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import WeatherBox from './component/WeatherBox';
+import WeatherButton from './component/WeatherButton';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+// 1. when app opens, current location weather is rendered
+// 2. current location, temperature and status
+// 3. there are 5 buttons (1 current location and 4 others)
+// 4. when location is selected, selected city's weather is shown
+// 5. when current location button is clicked, current location weather is shown 
+// 6. until data has been received, this is shown by loading spinner
 function App() {
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position)=> {
+      let lat = position.coords.latitude
+      let lon = position.coords.longitude
+      getWeatherByCurrentLocation(lat, lon)
+    });
+  }
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9c50d93799569302b94e2ab396b348f9`
+    let response = await fetch(url)
+    let data = await response.json();
+    console.log("data", data)
+  }
+
+  useEffect (() => {
+    getCurrentLocation()
+
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="container">
+      <WeatherBox/>
+      <WeatherButton/>
+      </div>
     </div>
   );
 }
---
+++ src/component/WeatherBox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const WeatherBox = () => {
+  return (
+    <div className="weather-box">
+        <div>Auckland</div>
+        <h2>30 degrees Celsius</h2>
+
+    </div>
+
+  )
+}
+
+export default WeatherBox
