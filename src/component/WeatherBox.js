def44a627b945a1d46e9e98137eb394f427d0cc8
Chan Woo SHIM <chanwooshim@Chans-MacBook-Air.local>
2024-08-18 23:58:09 +1200

    to one decimal place on Farenheit value

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -5,7 +5,7 @@ const WeatherBox = ({weather}) => {
   return (
     <div className="weather-box">
         <div>{weather?.name}</div>
-        <h2>{weather?.main.temp}C/ {(weather?.main.temp * 1.8 +32).toFixed(2)}F </h2>
+        <h2>{weather?.main.temp}C/ {(weather?.main.temp * 1.8 +32).toFixed(1)}F </h2>
         <h3>{weather?.weather[0].description}</h3>
 
     </div>
